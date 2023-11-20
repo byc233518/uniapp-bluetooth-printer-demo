a2597745c00dcf4736ace99cc109c4fa38c2f589
bai <byc233518@gmail.com>
2023-11-20 17:36:31 +0800

    [ fix ]: 自定义导航栏之后, 状态栏高度动态计算

--- pages/webview/index.vue
+++ pages/webview/index.vue
@@ -46,5 +46,23 @@
 				}
 			},
 		},
+		onLoad(op) {
+		  let height = 0; //定义动态的高度变量
+		  	let statusbar = 0; // 动态状态栏高度
+		  	uni.getSystemInfo({ // 获取当前设备的具体信息
+		  		success: (sysinfo) => {
+		  			statusbar = sysinfo.statusBarHeight;
+		  			height = sysinfo.windowHeight;
+		  		}
+		  	});
+		  	let currentWebview = this.$scope.$getAppWebview(); //获取当前web-view
+		  	setTimeout(function() {
+		  		var wv = currentWebview.children()[0];
+		  		wv.setStyle({ //设置web-view距离顶部的距离以及自己的高度，单位为px
+		  			top: statusbar, //此处是距离顶部的高度，应该是你页面的头部
+		  			height: height, //webview的高度
+		  		})
+		  	}, 200); //如页面初始化调用需要写延迟
+		},
 	}
 </script>
